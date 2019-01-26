9ba6e5b289c89dc23dc1aa004fd0e4d63f3d6bf2
snarksliveshere <snarksliveshere@gmail.com>
2019-01-26 12:42:58 +0300

    initial commit

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Text('Login Screen'),
+    );
+  }
+}
